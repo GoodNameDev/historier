787e9fbf79953368ce92f72d471819e882dd71a3
Mr.GIitch <philiprouthe@gmail.com>
2022-10-09 13:16:34 +0200

    Påbegyndt historie hjemmeside

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ include 'config.php'
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/about.css">
     <link rel="icon" type="image/x-icon" href="images/logo.png">
     <title><?php echo $name?>- Hjem</title>
   </head>
@@ -14,12 +14,5 @@ include 'config.php'
     <b id="beta"><?php if ($devmode == True) {
       print "The site is currently in dev mode, the version is " . $pagevertion;
     } ?></b>
-    <div class="choose">
-      <a href="børne/index.php">Børne historier</a>
-      <a href="gyser/index.php">Gyser historier</a>
-      <a href="sjove/index.php">Sjove historier</a>
-      <a href="about.php">Om os</a>
-    </div>
-
   </body>
 </html>
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Historier "; //Hjemmeside navn (uden .dk, .com, .net esc.)
+$devmode = True;
+$pagevertion = "1.0.0"
+
+
+
+
+ ?>
